da9fa0890091692ddf7a710a9980eca509992854
leumij <cjimuel74@gmail.com>
2024-01-26 09:16:40 +0800

    pseudocode

---
+++ Calculate.py
@@ -0,0 +1,8 @@
+# Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+#pseudocode
+
+# Ask user for input 
+# Calculate for user output
+# If product is equal 1000 return product
+# If less than 1000 return sum

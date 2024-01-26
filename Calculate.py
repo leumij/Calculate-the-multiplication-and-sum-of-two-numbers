2ff0ca7b7b0ac05c2b5f498b628c7f9dfdbbb0c0
leumij <cjimuel74@gmail.com>
2024-01-26 09:34:25 +0800

    Final

--- Calculate.py
+++ Calculate.py
@@ -9,11 +9,11 @@ def multiplication_or_sum(number_1, number_2):
 # If product is equal 1000 return product
     if product <= 1000:
      return product
-
+# If greater than 1000 return sum
     else:
         return number_1 + number_2
 
-
+# Print the result
 first = multiplication_or_sum(20,30)
 print("The result is", first)
 
